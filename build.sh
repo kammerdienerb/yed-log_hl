c4b2c74f2ef425366948beebe315e54a63d54b9a
Brandon Kammerdiener <kammerdienerb@MacBook-Pro-330.local>
2024-07-03 14:15:18 -0400

    pcre2

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-gcc -o log_hl.so log_hl.c $(yed --print-cflags) $(yed --print-ldflags)
+
+PCRE2_FLAGS=""
+if which pcre2-config > /dev/null; then
+    PCRE2_FLAGS="$(pcre2-config --cflags-posix --libs-posix) -DYED_SYNTAX_USE_PCRE2"
+fi
+
+gcc -o log_hl.so log_hl.c $(yed --print-cflags --print-ldflags) ${PCRE2_FLAGS}
